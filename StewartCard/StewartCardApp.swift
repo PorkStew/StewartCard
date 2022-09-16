7193119e445a7c10f58cbd4047e44258e3bdc3c1
PorkStew <stewartclay166@gmail.com>
2022-09-16 01:51:22 -0700

    Initial Commit

---
+++ StewartCard/StewartCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  StewartCardApp.swift
+//  StewartCard
+//
+//  Created by Stewart Clay on 2022/09/16.
+//
+
+import SwiftUI
+
+@main
+struct StewartCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
